5e75e56f14cc118928570577fcea9a599663fa4a
martinz2002 <bcsyztt@outlook.com>
2024-05-07 22:35:47 +0800

    Update and rename srv_info_private to srv_info_private.h

--- OS_Tasks/srv_info_private.h
+++ OS_Tasks/srv_info_private.h
@@ -1,4 +1,7 @@
 // Modify this file according to your WIFI SSID and MQTT Broker Configuration.
+#ifndef __SRV_INFO_PRIVATE_H
+#define __SRV_INFO_PRIVATE_H
+
 #define WIFI_SSID "EXAMPLE_WIFI"
 #define WIFI_PWD  "88888888"
 
@@ -6,3 +9,5 @@
 #define MQTT_PORT 1883
 #define MQTT_USER "mqtt_username"
 #define MQTT_PWD "mqtt_password"
+
+#endif // __SRV_INFO_PRIVATE_H
